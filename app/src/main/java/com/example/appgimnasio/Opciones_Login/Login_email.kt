0a89bfa36dbad057d0380c6956bea1c9fcac53d7
JoseDiegoSD <josediegosd13@gmail.com>
2026-09-09 10:22:38 -0700

    AvanceClaseDia8/9

--- app/src/main/java/com/example/appgimnasio/Opciones_Login/Login_email.kt
+++ app/src/main/java/com/example/appgimnasio/Opciones_Login/Login_email.kt
@@ -1,7 +1,9 @@
 package com.example.appgimnasio.Opciones_Login
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,18 +11,73 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.appgimnasio.R
 import com.example.appgimnasio.Registro_email
 import com.example.appgimnasio.databinding.ActivityLoginEmailBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login_email : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginEmailBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginEmailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere porfavor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.BtnIngresar.setOnClickListener {
+            validarInfo()
+        }
+
         binding.TxtRegistrarme.setOnClickListener {
             startActivity(Intent(this@Login_email, Registro_email::class.java))
         }
+    }
+
+    private var email = ""
+    private var password = ""
+    private fun validarInfo() {
+        email = binding.EtEmail.text.toString().trim()
+        password = binding.EtPassword.text.toString().trim()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.EtEmail.error = "Email invalido"
+            binding.EtEmail.requestFocus()
+        }
+        else if (email.isEmpty()){
+            binding.EtEmail.error = "Ingrese un email"
+            binding.EtEmail.requestFocus()
+        }
+        else if (password.isEmpty()){
+            binding.EtPassword.error = "Ingrese el password"
+            binding.EtPassword.requestFocus()
+        }
+        else{
+            loginUsuario()
+        }
+
+
+    }
+
+    private fun loginUsuario() {
+        progressDialog.setMessage("Ingresando")
+        progressDialog.show()
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+
+            }
+            .addOnFailureListener {
+
+            }
+
+
     }
 }
